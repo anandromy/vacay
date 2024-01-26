c75782b876d566ad98b3730cda65b19a3fa6de67
anandromy <23f3003270@ds.study.iitm.ac.in>
2024-01-26 14:43:57 +0530

    UI: Account dropdown

--- client/src/components/account.tsx
+++ client/src/components/account.tsx
@@ -1,12 +1,37 @@
+import { UserIcon } from "lucide-react"
 import { Button } from "./ui/button"
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuItem, DropdownMenuGroup} from "./ui/dropdown-menu"
+import { Link } from "react-router-dom"
 
 export const Account = () => {
-
-    // TODO: Make Account a dropdown menu with profile, logout, my hotels/admin profile etc.
     return(
-       <>
-        <Button className="hidden lg:block rounded-lg">Account</Button>
-        <Button className="lg:hidden rounded-full">A</Button>
-       </>
+       <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Button className="rounded-full p-2">
+                    <UserIcon />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup className="space-y-2">
+                    <DropdownMenuItem>
+                        <Link to="/dashboard">
+                            Dashboard
+                        </Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        <Link to="/profile">
+                            Profile
+                        </Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        <button>
+                            Sign out
+                        </button>
+                    </DropdownMenuItem>
+                </DropdownMenuGroup>
+            </DropdownMenuContent>
+       </DropdownMenu>
     )
 }
