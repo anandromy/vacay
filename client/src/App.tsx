423c61ed82fa6139025f558fbff6790b01e2214d
anandromy <23f3003270@ds.study.iitm.ac.in>
2024-05-08 08:53:14 +0530

    UI: Layout of header and main, hero is inside main

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import { Header } from "./components/header"
+import { Hero } from "./components/hero"
 
 function App() {
 
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-[url('/hero.jpg')] text-white bg-cover bg-center">
       <Header />
+      <main className="max-w-[85rem] mx-auto w-full p-6 flex-1 flex flex-col justify-center">
+        <Hero />
+      </main>
     </div>  
   )
 }
