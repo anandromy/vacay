2cb09babe90c6185a0c3355b6c78f932533af20a
anandromy <23f3003270@ds.study.iitm.ac.in>
2024-05-16 20:39:48 +0530

    setup: cloudinary

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,13 @@ import { userRouter } from './routes/user'
 import { authRouter } from './routes/auth'
 // Parses cookies, without it req.cookies would be
 import cookieParser from "cookie-parser"
+import { v2 as cloudinary } from "cloudinary"
 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 const app = express()
 
