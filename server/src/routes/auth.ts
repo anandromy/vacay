1a8966eb8793c80eccc38e2f984ebb3076a6982d
anandromy <23f3003270@ds.study.iitm.ac.in>
2024-05-16 10:36:24 +0530

    middleware api endpoint

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -2,6 +2,9 @@ import express, {Request, Response} from "express"
 import { UserSchema } from "common-vacay"
 import { User } from "../models/user"
 import bcrypt from "bcryptjs"
+import { generateToken } from "../middleware/generateToken"
+import ms from "ms"
+import { verifyToken } from "../middleware/middleware"
 export const authRouter = express.Router()
 
 const loginData = UserSchema.partial({
@@ -24,9 +27,15 @@ authRouter.post("/login", async (req: Request, res: Response) => {
         if(!isMatch){
             return res.status(400).json({ message: "Invalid credentials" })
         }
+        const token = generateToken(`${userExists._id}`)
+        res.cookie("auth_token", token, { httpOnly: true, sameSite: "none", secure: true, maxAge: ms("2 days")})
         return res.status(200).json({ message: "Successfully logged in" })
     }catch(error){
         console.log("Error in login route", error)
         return res.status(500).json({ message: "Internal error"})
     }
+})
+
+authRouter.get("/validate_token", verifyToken, async (req, res) => {
+    res.status(200).json({ userId: req.userId })
 })
