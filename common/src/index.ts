833441f682c946424d212c021c3be0460f53a4bc
anandromy <23f3003270@ds.study.iitm.ac.in>
2024-05-16 19:55:49 +0530

    Hotel schema and type

--- common/src/index.ts
+++ common/src/index.ts
@@ -14,3 +14,36 @@ export const UserSchema = z.object({
     firstName: z.string().min(1),
     lastName: z.string().min(1)
 })
+
+export const HotelZodSchema = z.object({
+    userId: z.string(),
+    name: z.string(),
+    city: z.string(),
+    country: z.string(),
+    description: z.string(),
+    type: z.string(),
+    adultCount: z.number().min(1),
+    childCount: z.number(),
+    pricePerNight: z.number(),
+    starRating: z.number().min(1).max(5),
+    lastUpdated: z.date(),
+    facilities: z.array(z.string()),
+    imageUrls: z.array(z.string())
+})
+
+export type HotelModelType = {
+    _id: string
+    userId: string
+    name: string
+    city: string
+    country: string
+    description: string
+    type: string
+    adultCount: number
+    childCount: number
+    pricePerNight: number
+    starRating: number
+    lastUpdated: Date
+    facilities: string[]
+    imageUrls: string[]
+}
